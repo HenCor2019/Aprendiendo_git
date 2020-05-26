e17c73c06ae1923745fa741d970208585ffcfc5b
HenCor2019 <Henry200amaya@gmail.com>
2020-05-25 18:13:57 -0600

    Add: New version PreParcial

---
+++ Pre_Parcial/InitialForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+
+namespace Pre_Parcial
+{
+    partial class InitialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.administrador1 = new Pre_Parcial.Administrador();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.administrador1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(689, 430);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // administrador1
+            // 
+            this.administrador1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.SetColumnSpan(this.administrador1, 2);
+            this.administrador1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.administrador1.Location = new System.Drawing.Point(3, 3);
+            this.administrador1.Name = "administrador1";
+            this.tableLayoutPanel1.SetRowSpan(this.administrador1, 2);
+            this.administrador1.Size = new System.Drawing.Size(683, 424);
+            this.administrador1.TabIndex = 0;
+            // 
+            // InitialForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 430);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "InitialForm";
+            this.Text = "InitialForm";
+            this.Load += new System.EventHandler(this.InitialForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Pre_Parcial.Administrador administrador1;
+    }
+}
---
+++ Pre_Parcial/InitialForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Pre_Parcial
+{
+    public partial class InitialForm : Form
+    {
+        public InitialForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitialForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
