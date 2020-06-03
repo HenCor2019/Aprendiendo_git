0890541bb428ed38194bb33004df7e029055c96a
HenCor2019 <Henry200amaya@gmail.com>
2020-06-03 00:39:33 -0600

    Add: new game

---
+++ Arkanoid/Vista/Form1.cs
@@ -0,0 +1,53 @@
+﻿/*
+* Modificando weas
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        
+        private UserControl uc = new UserControl();
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            //uc = *ponganElUserQueQuierenProbar*;
+            this.panel1.Controls.Add(uc);
+        }
+        
+        protected override void WndProc(ref Message m)
+        {
+            const int wmSyscommand = 0x0112;
+            const int scMove = 0xF010;
+            const int wmNclbuttondblclk = 0x00A3;
+
+            switch(m.Msg)
+            {
+                case wmSyscommand:
+                    int command = m.WParam.ToInt32() & 0xfff0;
+                    if (command == scMove)
+                        return;
+                    break;
+            }
+            
+            if (m.Msg == wmNclbuttondblclk)
+            {
+                m.Result = IntPtr.Zero;
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
