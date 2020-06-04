41c6eff00b3302e6c147794c9083b3cd1b7a6c94
HenCor2019 <Henry200amaya@gmail.com>
2020-06-03 23:39:06 -0600

    add: new userControl

---
+++ Arkanoid/Vista/GoodMorning.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class GoodMorning : UserControl
+    {
+        public GoodMorning()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
