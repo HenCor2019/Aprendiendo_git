9f40abfb02e037bf5e6bfca1f25cccb0055dc37d
Sugeyth <00143619@uca.edu.sv>
2020-06-04 11:32:32 -0600

    Gus Morining

---
+++ Arkanoid/Vista/Buenosdias6.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+
+namespace Arkanoid
+{
+    partial class Buenosdias6
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Italic,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (192)))),
+                ((int) (((byte) (192)))));
+            this.label1.Location = new System.Drawing.Point(48, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 138);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buenos dias\r\n06/2020";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Buenosdias6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Arkanoid.Properties.Resources.Untitled_design__5_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Buenosdias6";
+            this.Size = new System.Drawing.Size(450, 288);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Arkanoid/Vista/Buenosdias6.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Buenosdias6 : UserControl
+    {
+        public Buenosdias6()
+        {
+            InitializeComponent();
+        }
+    }
+}
